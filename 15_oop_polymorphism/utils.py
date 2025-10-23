0e813d884546b58687ef4f9351216833eb6963f5
mossadhagos <155261758+mossadhagos@users.noreply.github.com>
2025-10-23 18:24:37 +0200

    Update utils.py

--- 15_oop_polymorphism/utils.py
+++ 15_oop_polymorphism/utils.py
@@ -1,6 +1,5 @@
 from numbers import Number
  
 def validet_numbers(value) -> None:
-    for numbers in numbers:
-        if not isinstance(numbers, Number):
-            raise TypeError(f"elements must be numbers not {type(numbers)}")
+    if not isinstance(value, Number):
+        raise TypeError(f"elements must be numbers not {type(value)}")
